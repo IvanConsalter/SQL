e171bfae41c7a015f9c946502400ec080b7356a4
Ivan Consalter <ivanconsalter1@gmail.com>
2022-12-24 12:38:41 -0300

    insert, select pedido_venda

--- pedido-venda/pedido_venda.sql
+++ pedido-venda/pedido_venda.sql
@@ -20,4 +20,43 @@ create table pedido(
     valor_total decimal(10,2) not null
 ) engine=InnoDB;
 
+insert into cliente
+	(nome, email, data_nascimento)
+values
+	('Maria da Silva', 'maria@maria.com', '1978-08-11');
+    
+insert into cliente
+	(nome, email, data_nascimento)
+values
+	('José Bernades', 'jose@jose.com', '1982-02-18');
+    
+insert into cliente
+	(nome, email, data_nascimento)
+values
+	('Roberta de Fátima', 'roberta@roberta.com', '1984-06-06');
+    
+select * from cliente;
+select nome, email from cliente;    
+
+insert into pedido
+	(data_criacao, observacao, data_entrega, valor_frete, valor_total)
+values
+	('2022-12-22 12:00:00', 'Perto do Mercado da Alva', '2022-12-24', 15.00, 45.00);
+
+insert into pedido
+	(data_criacao, data_entrega, valor_frete, valor_total)
+values
+	('2022-12-15 12:00:00', '2023-01-02', 32.00, 145.00);
+    
+insert into pedido
+	(data_criacao, data_entrega, valor_frete, valor_total)
+values
+	('2022-12-02 15:42:00', '2022-12-27', 12.00, 85.00);
+    
+select * from pedido;
+select valor_frete, valor_total from pedido;
+
+
+
+
 
